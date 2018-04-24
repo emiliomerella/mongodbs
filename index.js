39c1da907576ca23b9497142a1bc64c5c522a83d
emiliomerella <emilio.merella@gmail.com>
2018-04-24 09:54:35 +0200

    conditional option to use dbs in req; check app is an express app
    the module will use the addDbsToReq option to add or not mongodbs instances into the express request.
    The module also check that app is a function as well as the app.use

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+const assert = require('assert');
+
 const connect = require('./lib/connect');
 const begin = require('./lib/beginware');
 const out = require('./lib/outware');
 
-module.exports = (app, configuration) =>
+module.exports = (app, configuration, options) =>
   new Promise(async (resolve, reject) => {
+    if (!options) {
+      options = {
+        addDbsToReq: true,
+      };
+    }
+
     try {
+      assert.equal('function', typeof app);
+      assert.equal('function', typeof app.use);
+
       const conf = configuration;
       const dbs = await connect(conf);
-      app.use(begin(dbs));
+
+      if (options.addDbsToReq) {
+        app.use(begin(dbs));
+      }
 
       return resolve(dbs);
     } catch (e) {
